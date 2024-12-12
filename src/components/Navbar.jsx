93aac5016826d7ace84a8191f9d339069ff2b505
Adel <adeleslami0077@yahoo.com>
2024-12-12 09:11:04 +0330

    Create And Add CharacterList & CharacterDetails Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-
+import {HeartIcon} from "@heroicons/react/24/outline"
 export default function Navbar() {
   return (
-    <div className="navbar">
-        <div className="navbar__logo">
-            dfdaf
-        </div>
-    </div>
+   <nav className="navbar">
+    <div className="navbar__logo">LOGO</div>
+    <input type="text" className="text-field" placeholder="search..."/>
+    <div className="navbar__result">Find X Character</div>
+    <button className="heart">
+      <HeartIcon className="icon"/>
+      <span className="badge">4</span>
+
+    </button>
+   </nav>
   )
 }
 
