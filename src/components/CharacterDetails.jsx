a3388eee43798b8436aac5dff27495d922ad0a1c
Adel <adeleslami0077@yahoo.com>
2024-12-14 13:09:44 +0330

    add create details to CharacterDetails component

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -1,8 +1,37 @@
+import { character } from "../../data/data";
 
 function CharacterDetails() {
   return (
-    <div>CharacterDetails</div>
-  )
+    <div style={{ flex: 1 }}>
+      <div className="character-detail">
+        <img
+          src={character.image}
+          alt={character.name}
+          className="character-detail__img"
+        />
+        <div className="character-detail__info">
+          <h3 className="name">
+            <span>{character.gender === "Male" ? "👨‍💼" : "👩‍💼"}</span>
+            <span>&nbsp;{character.name}</span>
+          </h3>
+          <div className="info">
+            <span className={`status ${character.status ==="Dead"? "red":""}`}></span>
+            <span>&nbsp;{character.status}</span>
+            <span> -&nbsp;{character.species}</span>
+          </div>
+          <div className="location">
+            <p>Last known location: </p>
+            <p>{character.location.name}</p>
+          </div>
+          <div className="action">
+            <button className="btn btn--primary">Add to Favourite</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="character-episodes"></div>
+    </div>
+  );
 }
 
-export default CharacterDetails
+export default CharacterDetails;
